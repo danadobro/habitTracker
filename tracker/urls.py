8032e3161ecaf8546b869c111067a42c67d9a5b6
danadobro <dobrosavljevicdana@gmail.com>
2025-05-21 05:02:31 +0400

    Displaying habits, mark as completed today functionality, progress bar

---
+++ tracker/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('add/', views.add_habit, name='add_habit'),
+    path('complete/', views.mark_complete, name='mark_complete'),
+
+]
